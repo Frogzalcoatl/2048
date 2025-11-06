eb6cfffc893ea9b9a650874bbf29f81438b4ede2
Youngboi3339800 <saharholmes@gmail.com>
2025-11-06 17:28:13 -0500

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
-    const sf::Font font("./assets/fonts/DejaVuSans.ttf");
-    string str = "Big Butts";
-    sf::Text text(font, str, 50);
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-        window.clear();
-        window.draw(text);
-        window.display();
-    }
+int main() {
+	auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+	window.setFramerateLimit(144);
+	const sf::Font font("./assets/fonts/DejaVuSans.ttf");
+	string str = "Big Butts";
+	sf::Text text(font, str, 50);
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
 }
