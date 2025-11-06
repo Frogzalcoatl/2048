8429adfd331e71393c32f08bd33ade35e79eb274
Youngboi3339800 <saharholmes@gmail.com>
2025-11-06 16:59:32 -0500

    Added assets folder

---
+++ src/game/board.cpp
@@ -0,0 +1,16 @@
+#include <array>
+using namespace std;
+
+class Board {
+    array<uint64_t, 4> arr = {1, 2, 3, 4};
+    array<array<uint64_t, 4>, 4> tiles = {{
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+    }};
+
+    void moveRight() {
+
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
-
+    const sf::Font font("./assets/fonts/DejaVuSans.ttf");
+    string str = "Big Butts";
+    sf::Text text(font, str, 50);
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -14,8 +18,8 @@ int main()
                 window.close();
             }
         }
-
         window.clear();
+        window.draw(text);
         window.display();
     }
 }
